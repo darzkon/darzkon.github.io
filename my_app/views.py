5440cdd8385c6e909f0bacb0687b82388fd218f6
darzkon <darzkon@gmail.com>
2020-08-20 12:48:30 +0500

    Changet Template

--- my_app/views.py
+++ my_app/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-   return render(request, 'index.html')
+   return render(request, 'my_app/index.html')
+
+def about(request):
+	return render(request, 'my_app/about.html')
+
+def login(request):
+	return render(request, 'auth/login.html')
